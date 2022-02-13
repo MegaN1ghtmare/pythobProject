239b9f282420e582d19e8522e5e0576a563befee
Vyacheslav <toslavik@gmail.com>
2022-02-13 11:19:32 +0200

    GCD func added

---
+++ src/gcd.py
@@ -0,0 +1,20 @@
+import math
+
+def gcd_function(x, y):
+    if y == 0:
+        return x
+    print('x =', x, 'y =', y)
+    return gcd_function(y, x % y)
+
+if __name__ == '__main__':
+    data = input()
+    data.strip()
+
+    data = data.split()
+    data = list(map(int, data))
+
+    x, y = data
+
+    print('GCD is: ', gcd_function(x, y))
+
+    print('Imported function said: ', math.gcd(x, y))
